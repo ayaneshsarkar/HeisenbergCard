9f83cc29af3fc51cac4a5f56fc32caf4be15fad2
Ayanesh Sarkar <ayaneshsarkar101@gmail.com>
2024-02-03 11:09:46 +0530

    Initial Commit

---
+++ HeisenbergCard/HeisenbergCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeisenbergCardApp.swift
+//  HeisenbergCard
+//
+//  Created by Ayanesh Sarkar on 03/02/24.
+//
+
+import SwiftUI
+
+@main
+struct HeisenbergCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
